bdd76d66d6d7465214a67122599589950ece0631
juliane1999 <lastjuliane@gmail.com>
2021-09-10 00:43:48 -0700

    fixed typos in migration table

---
+++ data/migrations/20210910070801_migration.js
@@ -0,0 +1,67 @@
+
+exports.up = function(knex) {
+    return knex.schema
+
+    .createTable('projects', table => {
+      table.increments('project_id')
+      .primary()
+      table.string('project_name')
+        .notNullable();
+        table.string('project_description')
+        table.boolean('project_completed')
+        .notNullable()
+        .defaultTo(false);
+    })
+
+    .createTable('resources', table => {
+      table.increments('resource_id')
+      .primary()
+      table.string('resource_name')
+        .unique()
+        .notNullable();
+        table.string('resource_description')
+    })
+
+    .createTable('tasks', table => {
+      table.increments('task_id')
+      .primary()
+      table.string('task_description')
+      .notNullable();
+      table.string('task_notes')
+      table.boolean('task_completed')
+      .notNullable()
+      .defaultTo(false);
+
+      table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onUpdate('CASCADE')
+    })
+
+    .createTable('project_resources', table => {
+
+      table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onUpdate('CASCADE')
+        
+      table.integer('resource_id')
+      .unsigned()
+      .notNullable()
+      .references('resource_id')
+      .inTable('resources')
+      .onUpdate('CASCADE')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('projects')
+      .dropTableIfExists('resources')
+      .dropTableIfExists('tasks')
+      .dropTableIfExists('project_resources');
+};
